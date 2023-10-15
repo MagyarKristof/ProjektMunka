a21b7c3610b01b9ede79ff9dd9acbab9dbeeedab
CicerGergely <145113384+CicerGergely@users.noreply.github.com>
2023-10-15 14:00:20 +0200

    Add files via upload

--- include/navbar_texts.php
+++ include/navbar_texts.php
@@ -1,14 +1,14 @@
 <?php
 $navbar = array(
-	"create_db" => array("file" => "create_db", "text" => "Adatbázis Létrehozása"),
-	"landing" => array("file" => "landing", "text" => "Kezdőlap"),
-	"landing2" => array("file" => "landing2", "text" => "Aloldal"),
-	"videos" => array("file" => "videos", "text" => "Videók"),
-	"gallery" => array("file" => "gallery", "text" => "Galéria"),
-	"upload" => array("file" => "upload", "text" => "Kép Feltöltése"),
-	"message" => array("file" => "message", "text" => "Üzenet Küldése"),
-	"disp_messages" => array("file" => "disp_messages", "text" => "Üzenetek megjelenítése"),
-	"login" => array("file" => "login", "text" => "Bejelentkezés")
+	"create_db" => array("file" => "create_db", "text" => "Adatbázis Létrehozása", "authorized" => false),
+	"landing" => array("file" => "landing", "text" => "Kezdőlap", "authorized" => true),
+	"landing2" => array("file" => "landing2", "text" => "Aloldal", "authorized" => true),
+	"videos" => array("file" => "videos", "text" => "Videók", "authorized" => true),
+	"gallery" => array("file" => "gallery", "text" => "Galéria", "authorized" => true),
+	"upload" => array("file" => "upload", "text" => "Kép Feltöltése", "authorized" => false),
+	"message" => array("file" => "message", "text" => "Üzenet Küldése", "authorized" => false),
+	"disp_messages" => array("file" => "disp_messages", "text" => "Üzenetek megjelenítése", "authorized" => false),
+	"login" => array("file" => "login", "text" => "Bejelentkezés", "authorized" => true)
 
 
 );
